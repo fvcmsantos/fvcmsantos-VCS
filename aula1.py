7573982f361ca673e6d871f04bfe4155b2fe5937
fvcmsantos <138405868+fvcmsantos@users.noreply.github.com>
2024-06-24 07:54:44 -0300

    Update aula1.py
    Teste de retorno no comando da linha 3

--- aula1.py
+++ aula1.py
@@ -1,6 +1,6 @@
 #Comando print (serve para mostrar o resultado dos comandos informados)
 
-print("Aula1 outubro 2023 TESTE GITHUB123456")
+print("Aula1 outubro 2023 TESTE")
 
 #variaveis
 #TEXTO => STRING(STR)
